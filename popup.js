80185057a620ae2eb31476693370525e7834164c
M-Ashford <ruroland@126.com>
2017-07-06 09:12:34 +0800

    1

---
+++ popup.js
@@ -0,0 +1,9 @@
+//we need to get inner html and show it
+
+var clock_div = document.getElementById('clock_div');
+
+
+
+//chrome.runtime.sendMessage('hello',function(response){
+    //    document.write(response)
+    //})
